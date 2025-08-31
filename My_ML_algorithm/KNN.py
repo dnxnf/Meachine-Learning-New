cca32c1262b897b9bd3382642ca139bac8a39803
dnxnf <144653417+dnxnf@users.noreply.github.com>
2025-08-31 21:25:39 +0800

    更新 .gitignore 规则

---
+++ My_ML_algorithm/KNN.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''
+@Project     ：MachineLearning 
+@File        ：KNN.py
+@Description ：
+@Author      ：Hello World
+@Date        ：2025/8/30 下午2:50 
+'''
---
+++ someProject/LLaVA/LLaVALearn.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''
+@Project     ：MachineLearning 
+@File        ：LLaVALearn.py
+@Description ：
+@Author      ：Hello World
+@Date        ：2025/8/31 下午3:31 
+'''
